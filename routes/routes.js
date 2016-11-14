ed8a8f825cb4244fd50457fbaa7e2507c5b0b4fb
mannutech <hiteshgoel.afs@gmail.com>
2016-11-14 06:37:44 +0530

    cache parameter next stage

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,50 @@
 var appRouter = function(app) {
 
-	var apicache = require('apicache').options({ debug: true }).middleware;
-	app.post("/api/v0/rate", apicache('10 seconds'),function(req, res) {
+	var api = require('apicache').options({ debug: true });
+	var realcache=api.middleware;
+	function onlyParameter(req,res)
+			{
+				var response =api.getIndex();
+				
+				var res=response['groups'];//Object.keys(response[1]);
+				console.log(res[0]);
 
+				if(req.body.SourceCurrency!==response.groups[0])
+				{
+						//api.clear(response.groups[0]);	
+						req.apicacheGroup=req.body.SourceCurrency;
+						return true;
+				}
+				else
+					return false;
+				/*if(typeof(req.apicacheGroup)==="undefined")
+				{
+					req.apicacheGroup=req.body.SourceCurrency;	//intialize cachegroup
+					//console.log(req.apicacheGroup);
+					
+					return true;
+				}
+				else
+				{	
+					if(req.apicacheGroup==req.body.SourceCurrency)
+					{
+						return false;
+					}
+					
+				else
+				{
+					api.clear(req.body.SourceCurrency);	
+					req.apicacheGroup=req.body.SourceCurrency;
+					//var indexing=api.getIndex();
+					//console.log(indexing);
+					return true;
+				}
+				}*/
+			}
+											 
+			
+	app.post("/api/v0/rate", realcache('10 seconds',onlyParameter),function(req, res) {
+		
 		if (req.method=='POST') 
 		{
 			/*	if(req.body.SourceCurrency ==='' && req.body.Amount ==='') 
@@ -22,10 +64,14 @@ var appRouter = function(app) {
 			{
 
 			}
-			*/
+			
+			if(req.body.SourceCurrency !== 'USD'||'GBP'||'CAD')
+			{
+				res.send(400);
+			}*/
+			
 			if (req.body.SourceCurrency !=='' && req.body.Amount !=='') 
 			{
-					console.log(req);
 					var db = require("./db.js");
 					db.db(req.body,function callback(err,result){
 							if (err) {
@@ -40,6 +86,7 @@ var appRouter = function(app) {
 								req.body.returncode=1;
 								var responseToSend=createResponse(result,req.body); 	
 								var JSONresponse=JSON.stringify(responseToSend);
+
 								 res.setHeader('Content-Type', 'application/json');
 								res.json(responseToSend);
 							}
@@ -68,12 +115,14 @@ var appRouter = function(app) {
 								req.body.returncode=1;
 								var responseToSend=createResponse(result,req.body); 	
 								var JSONresponse=JSON.stringify(responseToSend);
+								
 								 res.setHeader('Content-Type', 'application/json');
 								res.json(responseToSend);
 							}
 							//Function call for response structuring
 							//console.log(result);
                     }); 
+
 			}
 	/*		
 	function HandleIncomingRequest(req.body)
@@ -114,6 +163,7 @@ var appRouter = function(app) {
 		res.status = 400;
 		res.end();	    	
 		*/
+
 		}
 		else //CHECk for is POST request
 		{
